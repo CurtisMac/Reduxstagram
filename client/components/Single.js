030f7708cf833980d102dd5ed5c44095c8eb27d1
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-04-15 12:17:49 -0700

    finished posts reducer

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import Photo from './Photo'
+import Comments from './Comments'
+//import comments
 
 export default class Single extends Component {
-    render() {
-        return (
-            <div className='single-photo'>
-                Single Photo
-            </div>
-        )
-    }
+  render() {
+    //index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i]
+    console.log(post)
+    //get the post
+    return (
+      <div className='single-photo'>
+        <Photo i={i} post={post} {...this.props} />
+        <Comments /> 
+      </div>
+    )
+  }
 }
