4bd39f1ac6f50290c838ec001cc7d0d94ccaadb2
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-04-15 17:36:28 -0700

    added form

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -4,7 +4,7 @@ class Comments extends Component {
 
   renderComment(comment, i) {
     return (
-      <div className='comment'>
+      <div className='comment' key={i}>
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
@@ -14,14 +14,36 @@ class Comments extends Component {
     )
   }
 
+  handleSubmit(e){
+    e.preventDefault()
+    const postId = this.props.params.postId
+    const author = this.refs.author.value
+    const comment = this.refs.comment.value
+    this.props.addComment(postId, author, comment)
+  }
+
   render() {
     return (
       <div className='comments'>
         {this.props.postComments.map(this.renderComment)}
-        <form ref='commentForm' className='comment-form'>
-          <input type='text' ref='author' placeholder='Author'/>
-          <input type='text' ref='comment' placeholder='Comment' />
-          <input type='submit' hidden />
+        <form 
+          ref='commentForm'
+          className='comment-form'
+          onSubmit={this.handleSubmit.bind(this)}>
+          <input 
+            type='text' 
+            ref='author'
+            placeholder='Author' 
+          />
+          <input 
+            type='text' 
+            ref='comment'
+            placeholder='Comment' 
+          />
+          <input 
+            type='submit' 
+            hidden 
+          />
         </form>
       </div>
     )
